e214fa1132461ab8f439bbb7679fbd3aad9ee973
Evgeniy Sedov <sedoff-online@yandex.ru>
2024-04-10 21:59:55 +0700

    adds web server

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,82 @@
 package app
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"stats-of/internal/config"
+	"stats-of/internal/entities"
+	"stats-of/internal/healthz"
+)
+
+var appInfo = &entities.AppInfo{}
+
+type App struct {
+	server *http.Server
+}
+
+func New(config *config.Config) (*App, error) {
+	const (
+		defaultHTTPServerWriteTimeout = time.Second * 15
+		defaultHTTPServerReadTimeout  = time.Second * 15
+	)
+
+	app := new(App)
+	mux := http.NewServeMux()
+
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/healthz", healthz.MakeHandler(appInfo))
+
+	app.server = &http.Server{
+		Handler:      mux,
+		Addr:         ":" + strconv.Itoa(config.ServerPort),
+		WriteTimeout: defaultHTTPServerWriteTimeout,
+		ReadTimeout:  defaultHTTPServerReadTimeout,
+	}
+
+	return app, nil
+}
+
+func (a *App) Run() error {
+	err := a.server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return fmt.Errorf("server was stop with err: %w", err)
+	}
+
+	return nil
+}
+
+func (a *App) stop(ctx context.Context) error {
+	err := a.server.Shutdown(ctx)
+	if err != nil {
+		return fmt.Errorf("server was shutdown with error: %w", err)
+	}
+
+	return nil
+}
+
+func (a *App) GracefulStop(serverCtx context.Context, sig <-chan os.Signal, serverStopCtx context.CancelFunc) {
+	<-sig
+	var timeOut = 30 * time.Second
+	shutdownCtx, shutdownStopCtx := context.WithTimeout(serverCtx, timeOut)
+
+	go func() {
+		<-shutdownCtx.Done()
+		if shutdownCtx.Err() == context.DeadlineExceeded {
+			os.Exit(1)
+		}
+	}()
+
+	err := a.stop(shutdownCtx)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	serverStopCtx()
+	shutdownStopCtx()
+}
---
+++ internal/healthz/endpoint.go
@@ -0,0 +1,33 @@
+package healthz
+
+import (
+	"fmt"
+	"net/http"
+
+	"stats-of/internal/entities"
+	"stats-of/internal/utils"
+)
+
+type response struct {
+	Name         string `json:"name"`
+	BuildVersion string `json:"build_version"`
+	BuildTime    string `json:"build_time"`
+	GitTag       string `json:"git_tag"`
+	GitHash      string `json:"git_hash"`
+}
+
+func MakeHandler(info *entities.AppInfo) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		response := &response{
+			Name:         info.Name,
+			BuildVersion: info.BuildVersion,
+			BuildTime:    info.BuildTime,
+			GitTag:       info.GitTag,
+			GitHash:      info.GitHash,
+		}
+		err := utils.SuccessRespondWith200(w, struct{}{})
+		if err != nil {
+			fmt.Printf("failed to decode response = %v, error = %v\n", response, err)
+		}
+	}
+}
