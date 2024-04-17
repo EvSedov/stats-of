3c4ff4b3c4caf492680ee7dacab97279deb56b85
ryazanovs <sergey.ryazanov@sfera.zone>
2024-04-17 12:54:11 +0300

    test(main): Add functionality that adds data from the dump to redis

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,43 +1,45 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
-	"stats-of/internal/app"
-	"stats-of/internal/config"
 	"stats-of/internal/logger"
-	"syscall"
+	"stats-of/internal/storagetestsutils"
+
+	"go.uber.org/zap"
 )
 
 func main() {
 	logger.InitLogger()
 	logger.Log.Info("reading config...")
 
-	config, err := config.LoadFromEnv()
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
+	// config, err := config.LoadFromEnv()
+	// if err != nil {
+	// 	logger.Log.Info("failed to read config")
+	// 	os.Exit(1)
+	// }
+
+	// serverCtx, serverStopCtx := context.WithCancel(context.Background())
+	// app, err := app.New(config)
+	// if err != nil {
+	// 	logger.Log.Info("failed to read config")
+	// 	os.Exit(1)
+	// }
+
+	// sig := make(chan os.Signal, 1)
+	// signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// go func() {
+	// 	app.GracefulStop(serverCtx, sig, serverStopCtx)
+	// }()
+
+	// err = app.Run()
+	// if err != nil {
+	// 	logger.Log.Info("failed to read config")
+	// 	os.Exit(1)
+	// }
+
+	// <-serverCtx.Done()
+
+	// Initialize and handle database operations
+	if err := storagetestsutils.HandleCsvToDb(); err != nil {
+		logger.Log.Fatal("Ошибка при обработке данных CSV", zap.Error(err))
 	}
-
-	serverCtx, serverStopCtx := context.WithCancel(context.Background())
-	app, err := app.New(config)
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
-	}
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		app.GracefulStop(serverCtx, sig, serverStopCtx)
-	}()
-
-	err = app.Run()
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
-	}
-
-	<-serverCtx.Done()
 }
