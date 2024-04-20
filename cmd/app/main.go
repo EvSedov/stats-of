c3810a7c261899c27e11e192f133c476bd4943c3
ryazanovs <sergey.ryazanov@sfera.zone>
2024-04-20 17:46:56 +0300

    refactor(app): Move RunApp() function to internal/app package

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,43 +1,17 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
 	"stats-of/internal/app"
-	"stats-of/internal/config"
 	"stats-of/internal/logger"
-	"syscall"
+
+	"go.uber.org/zap"
 )
 
 func main() {
 	logger.InitLogger()
-	logger.Log.Info("reading config...")
+	logger.Log.Info("Starting application and reading configuration...")
 
-	config, err := config.LoadFromEnv()
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
+	if err := app.RunApp(); err != nil {
+		logger.Log.Fatal("Error occurred", zap.Error(err))
 	}
-
-	serverCtx, serverStopCtx := context.WithCancel(context.Background())
-	app, err := app.New(config)
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
-	}
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
-	go func() {
-		app.GracefulStop(serverCtx, sig, serverStopCtx)
-	}()
-
-	err = app.Run()
-	if err != nil {
-		logger.Log.Info("failed to read config")
-		os.Exit(1)
-	}
-
-	<-serverCtx.Done()
 }
